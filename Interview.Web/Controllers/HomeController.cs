072e4450360eb8f9a3e927e8b756f05c4dbb5e44
Cody Higbee <ravenclawmage@users.noreply.github.com>
2015-04-09 23:15:16 -0600

    Initial practice interview project imported & converted from an old MVC 2 project

---
+++ Interview.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interview.Web.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+    }
+}
